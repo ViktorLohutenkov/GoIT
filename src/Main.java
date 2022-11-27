40daa58f7bfb2b40d97e5752a4dc44b7a1163c40
ViktorLohutenkov <viktor.lohutenkov@gmail.com>
2022-11-27 16:41:02 +0200

    base class for Shape

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,58 @@
-import java.util.Arrays;
+abstract class Shape{
+    public abstract String getName();
 
-class HarekCity {
+}
+class Circle extends Shape{
+    @Override
+    public String getName() {
+        return "Circle";
 
-    public char[][] createKeyboard() {
-
-        return ;
     }
+}
 
+class Quad extends Shape{
+    @Override
+    public String getName() {
+        return "Quad";
 
+    }
+}
+
+class Triangle extends Shape{
+    @Override
+    public String getName() {
+        return "Triangle";
+
+    }
+}
+
+class Star extends Shape {
+    @Override
+    public String getName() {
+        return "Star";
+
+    }
+}
+
+class Pentagon extends Shape{
+    @Override
+    public String getName() {
+        return "Pentagon";
+    }
+}
+class ShapeTest {
+    public String test(Shape shape){
+        return shape.getName();
+    }
     public static void main(String[] args) {
-        char[][] keyboard = new HarekCity().createKeyboard();
-        for (int i = 0; i < keyboard.length; i++) {
-            System.out.println(Arrays.toString(keyboard[i]));
 
-        }
+
+    ShapeTest shapeTest = new ShapeTest();
+        System.out.println(shapeTest.test(new Circle()));
+        System.out.println(shapeTest.test(new Quad()));
+        System.out.println(shapeTest.test(new Triangle()));
+        System.out.println(shapeTest.test(new Star()));
+        System.out.println(shapeTest.test(new Pentagon()));
+
     }
 }
