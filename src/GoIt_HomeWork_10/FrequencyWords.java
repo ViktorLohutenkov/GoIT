1aa98e83df991339f1a5838c7f3fd5d40ecdb48b
ViktorLohutenkov <viktor.lohutenkov@gmail.com>
2023-02-09 21:34:18 +0200

    Main.java

--- src/GoIt_HomeWork_10/FrequencyWords.java
+++ src/GoIt_HomeWork_10/FrequencyWords.java
@@ -1,12 +1,10 @@
 package GoIt_HomeWork_10;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class FrequencyWords {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Map<String, Integer> wordCount = new HashMap<>();
         try (InputStream file = new FileInputStream("words.txt");
              Scanner scan = new Scanner(file)) {
@@ -20,11 +18,19 @@ public class FrequencyWords {
                     }
                 }
             }
-        }catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
-                System.out.println(entry.getKey() + " " + entry.getValue());
-            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+        Comparator<String> comparator = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                Integer value1 = wordCount.get(o1);
+                Integer value2 = wordCount.get(o2);
+                return value1.compareTo(value2) > 0 ? 1 : -1;
+            }
+        };
+        Map<String, Integer> result = new TreeMap<>(comparator.reversed());
+        result.putAll(wordCount);
+        System.out.println(result);
     }
+}
