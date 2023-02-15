9b397f2041b8c65d16f812e70c8d94dcedb124b7
ViktorLohutenkov <viktor.lohutenkov@gmail.com>
2023-02-15 11:45:58 +0200

    Main.java

--- src/GoIt_HomeWork_11/ArrayReturnSort.java
+++ src/GoIt_HomeWork_11/ArrayReturnSort.java
@@ -2,18 +2,23 @@ package GoIt_HomeWork_11;
 
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ArrayReturnSort {
     public static void main(String[] args) {
-        List<String> array = Arrays.asList("1, 2, 0", "4, 5");
-        System.out.println(sortedArray(array));
+        String[] numbers = {"1, 2, 0", "4, 5"};
+        System.out.println(sortedArrayToNums(numbers));
     }
-        private static List<String> sortedArray(List<String> array) {
-            return array.stream()
-                    .map(string -> string.split(", "))
-                    .sorted()
-                    .collect(Collectors.toList());
+    public static String sortedArrayToNums(String[] numbers) {
+
+      return Stream.of(numbers)
+                .map(nums -> nums.split(", "))
+                .flatMap(Arrays::stream)
+                .sorted(Comparator.comparingInt(Integer::parseInt))
+                .collect(Collectors.joining(", "));
     }
 }
--- src/GoIt_HomeWork_11/ReturnListNames.java
+++ src/GoIt_HomeWork_11/ReturnListNames.java
@@ -1,24 +1,24 @@
 package GoIt_HomeWork_11;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class ReturnListNames {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Viktor", "Alex", "Oleg", "Sergey", "Ivan", "Petro");
+        List<String> names = List.of("Viktor", "Alex", "Oleg", "Sergey", "Ivan", "Petro");
 
         System.out.println(oddIndexList(names));
         System.out.println(sortedNamesToUpperCase(names));
     }
     public static List<String> oddIndexList(List<String> names) {
-        IntStream.range(0, names.size())
-                .filter(index -> index % 2 != 0)
-                .forEach(position -> System.out.println(position + ". " + names.get(position)));
-    return names;
-}
+        return IntStream.range(0, names.size())
+                .filter(index -> index % 2 == 1)
+                .mapToObj(names::get)
+                .collect(Collectors.toList());
+    }
     private static List<String> sortedNamesToUpperCase(List<String> names) {
         return names.stream()
                 .map(String::toUpperCase)
